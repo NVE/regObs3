d1d5fecec51778f3a2acb2a12e9ccb203d7c402d
ingljo <ingljo@gmail.com>
2017-02-10 13:55:37 +0100

    Renamed folder icedangerobs

---
+++ www/app/ice/iceregistration/icedangerobs/iceDangerObsCtrl.js
@@ -0,0 +1,17 @@
+angular
+    .module('RegObs')
+    .controller('IceDangerObsCtrl', function IceDangerObsCtrl($scope) {
+
+        var vm = this;
+
+        vm.areaArray = [
+            "Ikke gitt",
+            "Akkurat her",
+            "På denne siden av vannet",
+            "På dette vannet",
+            "Mange vann i nærheten"
+        ];
+
+        $scope.$on( '$ionicView.loaded', function(){} );
+
+    });
