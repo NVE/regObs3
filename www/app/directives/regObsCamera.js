cf1a1612a42046fb03a881ad9fbe939118f40693
ingljo <ingljo@gmail.com>
2017-05-30 14:25:58 +0200

    Fixed camera directive

--- www/app/directives/regObsCamera.js
+++ www/app/directives/regObsCamera.js
@@ -1,6 +1,6 @@
 angular
     .module('RegObs')
-    .directive('regobsCamera', function ($ionicPlatform, Pictures) {
+    .directive('regobsCamera', function ($ionicPlatform, Pictures, $state) {
         'ngInject';
 
         return {
@@ -11,55 +11,10 @@ angular
         };
 
         function link(scope) {
-
             scope.click = window.Camera && function () {
                     $ionicPlatform.ready(function () {
-                        //var options = {
-                        //    quality: 100,
-                        //    destinationType: Camera.DestinationType.FILE_URI,
-                        //    sourceType: Camera.PictureSourceType.CAMERA,
-                        //    allowEdit: false,
-                        //    encodingType: Camera.EncodingType.JPEG,
-                        //    targetWidth: 1200,
-                        //    targetHeight: 1200,
-                        //    popoverOptions: CameraPopoverOptions,
-                        //    saveToPhotoAlbum: true,
-                        //    correctOrientation: true
-                        //};
-
-                        //snapPic(options);
                         Pictures.getCameraPicture($state.current.data.registrationProp);
                     });
                 }
         }
-
-        //function snapPic(options) {
-        //    $cordovaCamera
-        //        .getPicture(options)
-        //        .then(function (imageUri) {
-        //            var pic = Pictures.addPicture($state.current.data.registrationProp, imageUri);
-        //            //image.src = "data:image/jpeg;base64," + imageData;
-        //            if (AppSettings.data.compass) {
-        //                setOrientation(pic);
-        //            }
-        //        }, function (err) {
-        //            // error
-        //        });
-        //}
-
-        //function setOrientation(pic) {
-        //    $cordovaDeviceOrientation
-        //        .getCurrentHeading()
-        //        .then(function (result) {
-        //            /*var trueHeading = result.trueHeading;
-        //             var accuracy = result.headingAccuracy;
-        //             var timeStamp = result.timestamp;*/
-        //            var magneticHeading = result.magneticHeading;
-        //            pic.Aspect = magneticHeading.toFixed(0);
-        //        }, function (err) {
-        //            // An error occurred
-        //            pic.Aspect = -1;
-        //        });
-        //}
-
     });
